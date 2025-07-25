0dd088b67923783fdf4d1c929d0a961b0737e264
Tiago Coimbra <adminy@tacs.pt>
2025-07-25 16:13:26 +0100

    added a TTL along with the value

--- src/Cache.ts
+++ src/Cache.ts
@@ -1,15 +1,27 @@
 type StorageKey = string
-type StorageValue = string
+type StorageValue = {
+	/** in seconds */
+	ttl: number
+	value: string
+}
 
 export class Cache {
 	private storage: Record<StorageKey, StorageValue> = {}
 
-	public get(key: StorageKey): StorageValue {
-		return this.storage[key]
+	public get(key: StorageKey): StorageValue['value'] {
+		return this.storage[key].value
 	}
 
-	public set(key: StorageKey, value: StorageValue) {
-		this.storage[key] = value
+	public set(key: StorageKey, value: StorageValue['value'], ttl?: number) {
+		ttl = (ttl ?? 60) * 1000
+		this.storage[key] = {
+			ttl,
+			value,
+		}
+
+		setTimeout(() => {
+			this.flush(key)
+		}, ttl)
 	}
 
 	public flush(key: StorageKey) {
