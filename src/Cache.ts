fbd0eb32f066e0a3ff477c3b75c7d61b219d3108
Tiago Coimbra <adminy@tacs.pt>
2025-07-25 16:32:05 +0100

    moved flusher to the constructor + replaced TTL with deleteAt timestamp | improved memory usage since it's not constantly creating setTimeouts

--- src/Cache.ts
+++ src/Cache.ts
@@ -1,27 +1,41 @@
 type StorageKey = string
 type StorageValue = {
-	/** in seconds */
-	ttl: number
+	/** timestamp */
+	deleteAt: number
 	value: string
 }
 
 export class Cache {
 	private storage: Record<StorageKey, StorageValue> = {}
 
+	constructor() {
+		setInterval(() => {
+			for (const key of Object.keys(this.storage)) {
+				if (this.isFlushable(key)) {
+					this.flush(key)
+				}
+			}
+		}, 1000)
+	}
+
+	private isFlushable(key: StorageKey): boolean
+	private isFlushable(val: StorageValue): boolean
+	private isFlushable(keyOrVal: StorageKey | StorageValue): boolean{
+		const value = typeof keyOrVal === 'string' ? this.storage[keyOrVal] : keyOrVal
+		return new Date().getTime() >= value.deleteAt
+	}
+
 	public get(key: StorageKey): StorageValue['value'] {
 		return this.storage[key].value
 	}
 
 	public set(key: StorageKey, value: StorageValue['value'], ttl?: number) {
-		ttl = (ttl ?? 60) * 1000
+		ttl = ttl ?? 60
+
 		this.storage[key] = {
-			ttl,
+			deleteAt: new Date().setSeconds(new Date().getSeconds() + ttl),
 			value,
 		}
-
-		setTimeout(() => {
-			this.flush(key)
-		}, ttl)
 	}
 
 	public flush(key: StorageKey) {
